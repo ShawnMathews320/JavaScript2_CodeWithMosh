aa1a7780a405542807ae1a9e0100d59ba67da991
shawn.mathews <shawn.mathews@stmartin.edu>
2022-06-21 05:23:31 -0700

    mixins

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+function mixin(target, ...sources) {
+	Object.assign(target, ...sources);
+}
+
+const canEat = {
+	eat: function () {
+		this.hunger--;
+		console.log('eating');
+	},
+};
+
+const canWalk = {
+	walk: function () {
+		console.log('walking');
+	},
+};
+
+const canSwim = {
+	swim: function () {
+		console.log('swim');
+	},
+};
+
+function Person() {}
+
+mixin(Person.prototype, canEat, canWalk);
+
+const person = new Person();
+console.log(person);
+
+function Goldfish() {}
+
+mixin(Goldfish.prototype, canEat, canSwim);
+
+const goldfish = new Goldfish();
+console.log(goldfish);
---
+++ prototypical_inheritance/main.js
@@ -0,0 +1,3 @@
+function HtmlElement() {}
+
+function HtmlSelectElement() {}
