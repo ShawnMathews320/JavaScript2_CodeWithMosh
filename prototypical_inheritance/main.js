ae4194b670331de464637dd9fd29887740f891fa
shawn.mathews <shawn.mathews@stmartin.edu>
2022-06-21 06:29:06 -0700

    exercise

--- prototypical_inheritance/main.js
+++ prototypical_inheritance/main.js
@@ -1,3 +1,24 @@
-function HtmlElement() {}
+function HtmlElement() {
+	this.click = function () {
+		console.log('clicked');
+	};
+}
 
-function HtmlSelectElement() {}
+HtmlElement.prototype.focus = function () {
+	console.log('focused');
+};
+
+function HtmlSelectElement(items = []) {
+	this.items = items || [];
+
+	this.addItem = function (item) {
+		this.items.push(item);
+	};
+
+	this.removeItem = function (item) {
+		this.items.splice(this.items.indexOf(item), 1);
+	};
+}
+
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
